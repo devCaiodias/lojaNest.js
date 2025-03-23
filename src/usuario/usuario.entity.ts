ade5fa5f1623f0bb76d1a870a619d1ee00dabd4e
devCaiodias <caiodiasmartins12@gmail.com>
2025-03-23 16:03:15 -0300

    criando  a primeira entidade

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,7 +1,28 @@
- 
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'usuario' })
 export class UsuarioEntity {
-    id: string
-    nome: string
-    email: string
-    senha: string
+  @PrimaryGeneratedColumn('uuid')  
+  id: string;
+
+  @Column({ name: 'nome', type: 'varchar', length: 100, nullable: false })
+  nome: string;
+  @Column({ name: 'email', type: 'varchar', length: 70, nullable: false })
+  email: string;
+  @Column({ name: 'senha', type: 'varchar', length: 255, nullable: false })
+  senha: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+  @UpdateDateColumn({ name: 'update_at' })
+  updatedAt: string;
+  @DeleteDateColumn({ name: 'delete_at' })
+  deletedAt: string;
 }
